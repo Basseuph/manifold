1d975facca7cced561793713e406ec033d6052c8
Ulrich Langenbach <ulrich.langenbach@beuth-hochschule.de>
2017-06-20 18:27:06 +0200

    ADDED: initial component to implement systemC interface with test tool stub

---
+++ simulator/smp/benchmark/testing/systemc/systemc_test.cpp
@@ -0,0 +1,13 @@
+// @author ulrich.langenbach@beuth-hochschule.de
+// 
+//
+//
+
+#include <iostream>
+// #include <ostream>
+
+// using namespace std;
+
+int main(int argc, char **argv) {
+    std::cout << "test it" << std::endl;
+}
